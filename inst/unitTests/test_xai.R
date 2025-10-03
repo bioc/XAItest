21cad29fe8de06b3e2e8e6a0d1f22bd55b99aa03
gui <ghislain.fievet@gmail.com>
2025-10-03 06:36:22 +0200

    rm test

--- inst/unitTests/test_xai.R
+++ inst/unitTests/test_xai.R
@@ -23,7 +23,6 @@ test_XAItest <- function(){
         "LIME_feat_imp")))    
     p1 <- plotModel(results, "lm_pval", "feature1", "feature2")
     checkTrue("ggplot" %in% class(p1))
-    checkTrue('data' %in% names(p1))
     checkTrue('scales' %in% names(p1))
     checkTrue(length(p1)>0)
 
